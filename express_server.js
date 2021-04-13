0a2e93aa77fb6154550dfec2b2c6823dd8f08e15
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-04-12 19:49:41 -0600

    post request body to console

--- express_server.js
+++ express_server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-//const path = require("path");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
-//app.set('views', __dirname + '/views');
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -27,12 +30,26 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+function generateRandomString() {
+
+}
